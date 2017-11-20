b366475fa85a88b9896e92a2c260e57aa473f9ad
Vilann <jelle.santema@gmail.com>
2017-11-20 12:07:16 +0100

    Voeg toe login pagina

---
+++ login.php
@@ -0,0 +1,21 @@
+<h1>Log in</h1>
+<form action="verwerk.php" method="post">
+  <table>
+    <tr>
+      <td><label for="Naam">Naam: </label></td>
+      <td><input id="Naam" type="text" name="naam" value=""></td>
+    </tr>
+    <tr>
+      <td><label for="Wachtwoord">Wachtwoord: </label></td>
+      <td><input id="Wachtwoord" type="password" name="Wachtwoord" value=""></td>
+    </tr>
+  </table>
+  <br>
+  <input type="submit" name="Verstuur">
+
+  <p>
+    <b>Nog geen lid?</b> <br>
+    Wil jij lid worden van ZHTC? <br>
+    Klik <a href="registreer.php">hier</a> <!-- TODO: website invullen -->
+  </p>
+</form>
