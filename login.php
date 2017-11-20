cde0b8755761f927f49ce07600df46f3919c6670
Vilann <jelle.santema@gmail.com>
2017-11-20 12:07:16 +0100

    Voeg toe login pagina

---
+++ login.php
@@ -0,0 +1,14 @@
+<h1>Log in</h1>
+<form action="index.html" method="post">
+  <table>
+    <tr>
+      <td><label for="Naam">Naam: </label></td>
+      <td><input id="Naam" type="text" name="naam" value=""></td>
+    </tr>
+    <tr>
+      <td><label for="Wachtwoord">Wachtwoord: </label></td>
+      <td><input id="Wachtwoord" type="password" name="Wachtwoord" value=""></td>
+    </tr>
+  </table>
+  <input type="submit" name="Verstuur">
+</form>
