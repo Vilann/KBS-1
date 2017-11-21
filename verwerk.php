3f3e9950fdc089b53565ff2440f5848f08845ff5
Gentleman27 <32332886+Gentleman27@users.noreply.github.com>
2017-11-21 20:37:44 +0100

    moet het committen voordat ik kan pullen blijkbaar

--- verwerk.php
+++ verwerk.php
@@ -1,15 +1,21 @@
 <?php
 // Filter_input is een functie die kijkt of de informatie bestaat/aan (hier niet) gespecificeerde regels voldoet.
 if (($email = filter_input(INPUT_POST, 'email')) && ($ww = filter_input(INPUT_POST, 'wachtwoord'))) {
-    $db = "mysql:host=localhost;dbname=ZHTC;port=3306";
+    $db = "mysql:host=localhost;dbname=ZHTC;port=3306"; //de database opzoeken
     $user = "root";
     $pass = "";
     $pdo = new PDO($db, $user, $pass);
-    // We halen het wachtwoord op van het lid met het lidID dat bij het emailadres staat.
-    $stmt = $pdo->prepare("SELECT wachtwoord FROM login WHERE lidID=(SELECT lidID FROM lid WHERE ZHTC-emailadres = ?)");
+
+    // We halen het wachtwoord op van het lid met het lidID dat
+    // Bij het emailadres staat. met de volgende sql query:
+    $stmt = $pdo->prepare("SELECT wachtwoord FROM login WHERE lidID=
+      (SELECT lidID  FROM lid WHERE ZHTC-emailadres = ?)");
+
+    //Hier wordt de email voor het vraagteken in de query ingevuld.
     $stmt->execute(array($email));
+    //Het wachtwoord wat uit deze query komt wordt in de variabele
+    //dbww (database wachtwoord)gestopt.
     $dbww = $stmt->fetch();
-    // password_verify is een functie om een gehasht wachtwoord dat gemaakt is met password_hash() te checken
     if ($dbww['wachtwoord'] == password_verify($ww)) {
         session_start();
         $_SESSION['$email'] = $email;
@@ -17,7 +23,6 @@ if (($email = filter_input(INPUT_POST, 'email')) && ($ww = filter_input(INPUT_PO
 
     $pdo = null;
 }
-
 // Filter_input zorgt ervoor dat 1) de informatie gefilterd wordt en 2) de informatie 'veilig' is.
 
 if (isset($_SESSION['email'])) {
