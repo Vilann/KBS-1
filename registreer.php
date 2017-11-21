9658c64c8ec8759f1c32874acace24f401b707e0
Gentleman27 <32332886+Gentleman27@users.noreply.github.com>
2017-11-21 23:10:30 +0100

    bekijk de code, er staat uitleg, soms met een '?' dan n.o.t.k.

--- registreer.php
+++ registreer.php
@@ -5,27 +5,86 @@
     <?php include 'includes/header.php' ?>
 
     <h1>Registreer</h1>
-    <p>Hier kun je je inschrijven voor ZHTC. Bij opgave ga je akkoord met onze <b>privacyvoorwaarden</b> en ga je ermee akkoord dat we €25 per kwartaal van je rekening halen.</p>
+    <p>Hier kun je je inschrijven voor ZHTC. Bij opgave ga je akkoord met onze
+      <b>privacyvoorwaarden</b> en ga je ermee akkoord dat we &euro;25
+      <!-- http://www.homepage-maken.nl/htmlcursus/htmltekens.php NOTE: tip van de dag gr Kai-->
+      contributie per halfjaar van je rekening halen.</p>
 
     <form action="verwerk.php" method="post">
         <table>
+          <input type="hidden" name="lidID" value="1"> <?php // NOTE: Stiekem de het lidId meesturen? Gr Kai ?>
           <tr>
-            <td></td>
-            <td></td>
+            <td> Naam: </td>
+            <td> <input type="text" name="naam" placeholder="Voornaam"> </td>
           </tr>
           <tr>
-            <td></td>
-            <td></td>
+            <td> Tussenvoegsel: </td>
+            <td> <input type="text" name="tussenvoegsel" placeholder="Tussenvoegsel"> </td>
           </tr>
           <tr>
-            <td></td>
-            <td></td>
+            <td> Achternaam: </td>
+            <td> <input type="text" name="achternaam" placeholder="Achternaam"> </td>
           </tr>
           <tr>
-            <td></td>
-            <td></td>
+            <td> Geboortedatum: </td>
+            <td> <input type="date" name="Geboortedatum"> </td>
           </tr>
+          <tr>
+            <td> Adres: </td>
+            <td> <input type="text" name="adres" placeholder="Thomas á Kempisstraat 13"> </td>
+          </tr>
+          <tr>
+            <td> Postcode en woonplaats: </td>
+            <td> <input type="text" name="postcode" placeholder="1234 AB"> </td>
+            <td> <input type="text" name="woonplaats" placeholder="Zwolle"> </td>
+          </tr>
+          <tr>
+            <td> Geslacht: </td>
+            <td> <input type="radio" name="Man"> Man <br>
+                 <input type="radio" name="Vrouw"> Vrouw <br>
+                 <input type="radio" name="lgbt"> lgbt <br>
+                 <input type="radio" name="gevechtsheli"> gevechtshelikopter <br>
+                 <input type="radio" name="geen van bovenstaande"> geen van bovenstaande <br>
+            </td>
+          </tr>
+          <tr>
+            <td> Emaildres: </td>
+            <td> <input type="email" name="email" placeholder="lid@zhtc.nl"> </td>
+          </tr>
+          <tr>
+            <td> Rekeningnummer: </td>
+            <td> <input type="text" name="iban" placeholder="NL12RABO0123456789"> </td>
+          </tr>
+          <tr>
+            <td> Noodnummer: </td>
+            <td> <input type="tel" name="noodnummer" placeholder="06 123 45 678"> </td>
+          </tr>
+          <tr>
+            <td> T-shirtmaat: </td>
+            <td> <input type="radio" name="maatxs"> XS
+                 <input type="radio" name="maats"> S
+                 <input type="radio" name="maatm"> M
+                 <input type="radio" name="maatl"> L
+                 <input type="radio" name="maatxl"> XL
+                 <input type="radio" name="maatxxl"> XXL
+            </td>
+          </tr>
+          <tr>
+            <td> Medicatie: </td>
+            <td> <input type="text" name="noodnummer" placeholder="pillen en veel"> </td>
+          </tr>
+          <tr>
+            <td> Dieetwensen: </td>
+            <td> <input type="text" name="dieetwensen" placeholder="vlees en veel, lactose tollerant, liever kips leverworst dan gewone leverworst"> </td>
+          </tr>
+          <tr>
+            <td> Opmerking: </td>
+            <td> <input type="text" name="opmerking" placeholder="jullie hebben een hele coole getinte man als lid, wie is die toffe gozer?"> </td>
+          </tr>
+          <input type="hidden" name="ZHTC-emailadress" value="voornaam.achternaam@zhtc.nl">
+          <?php // TODO: de info voor de email ergens vandaan toveren Gr Kai ?>
         </table>
+        <?php // NOTE: door op deze knop te drukken wordt al deze info gestopt in de value registreer. *zie verwerk.php Gr Kai ?>
         <input type="submit" value="registreer" name="registreer">
     </form>
 
--- verwerk.php
+++ verwerk.php
@@ -1,28 +1,4 @@
 <?php
-// Filter_input is een functie die kijkt of de informatie bestaat.
-// en of de info aan (hier niet) gespecificeerde regels voldoet.
-if (($email = filter_input(INPUT_POST, 'email')) && ($ww = filter_input(INPUT_POST, 'wachtwoord'))) {
-
-    $db = "mysql:host=localhost;dbname=ZHTC;port=3306";
-    $user = "root";
-    $pass = "";
-    $pdo = new PDO($db, $user, $pass);
-
-    // We halen het wachtwoord op van het lid met het lidID dat
-    // Bij het emailadres staat. met de volgende sql query:
-    $stmt = $pdo->prepare("SELECT wachtwoord FROM login WHERE lidID=
-      (SELECT lidID  FROM lid WHERE ZHTC-emailadres = ?)");
-
-    //Hier wordt de email voor het vraagteken in de query ingevuld.
-    $stmt->execute(array($email));
-    //Het wachtwoord wat uit deze query komt wordt in de variabele
-    //dbww (database wachtwoord)gestopt.
-    $dbww = $stmt->fetch();
-    if ($dbww['wachtwoord'] == password_verify($ww)) {
-        session_start();
-        $_SESSION['$email'] = $email;
-    }
-
 /*
 Het registratieformulier en loginformulier hebben allebei een verstuurknop met een naam, login en registreer.
 De buitenste if-statements kijken welke van de 2 gebruikt is. Bij login wordt het eerste gebruikt, de loginfunctionaliteit.
@@ -30,36 +6,49 @@ Bij registreren wordt het tweede blok gebruikt.
  */
 
 if (isset($_POST['login'])) {
+    // allereerst wordt er gekeken of de email en het wachtwoord overeen komen.
     // Filter_input is een functie die kijkt of de informatie bestaat.
     // en of de info aan (hier niet) gespecificeerde regels voldoet.
     if (($email = filter_input(INPUT_POST, 'email')) && ($ww = filter_input(INPUT_POST, 'wachtwoord'))) {
-        // de database, de gebruikersnaam daarvoor en het wachtwoord worden
-        // in de volgende 3 variabelen gestopt.
+        // de database(db), de gebruikersnaam daarvoor en het wachtwoord worden
+        // in de variabelen(var) $db, $user en $pass gestopt.
+        // vervolgens maken we met "new PDO" een verbinding met de database en vullen we
+        // de credentials in die we in die variabelen hebben gestopt.
         $db = "mysql:host=localhost;dbname=ZHTC;port=3306";
         $user = "root";
         $pass = "";
-        //Er ontstaat een verbinding met de database (new PDO) en de variabele worden gebruikt.
         $pdo = new PDO($db, $user, $pass);
-        // We halen het wachtwoord op van het lid met het lidID dat bij het
-        // (bij login ingevulde) emailadres staat. en stoppen dat in de variabele stmt.
+
+        // Dan halen we het wachtwoord op van het lid met het lidID dat bij het
+        // (bij login ingevulde) emailadres staat. en stoppen dat in de var stmt.
         $stmt = $pdo->prepare("SELECT wachtwoord FROM login WHERE lidID=(SELECT lidID FROM lid WHERE ZHTC-emailadres = ?)");
-        //Hier wordt de email voor het vraagteken in de query ingevuld.
-        $stmt->execute(array($email));
-        // vervolgens st
-        $dbww = $stmt->fetch();
-        // password_verify is een functie om een gehasht wachtwoord dat gemaakt is met password_hash()
+        $stmt->execute(array($email));//Hier wordt de email voor het vraagteken in de query ingevuld.
+        $dbww = $stmt->fetch();//Het wachtwoord wat uit deze query komt wordt in de vardbww (database wachtwoord)gestopt.
+        // password_verify is een functie om een gehasht wachtwoord dat gemaakt is met password_hash() // NOTE: halve zin? gr Kai
         if ($dbww['wachtwoord'] == password_verify($ww)) {
             session_start();
             $_SESSION['$email'] = $email;
         }
 
-        $pdo = null;
+        $pdo = null; // hier wordt de db-verbinding weer verbroken.
     }
 }
-if (isset($_POST['registreer'])) {
+if (isset($_POST['registreer'])) {//als er op de registreer knop wordt gedrukt en alles is ingevuld? gr Kai
     // TODO: Logica toevoegen
+    // TODO: alle attributen in de db laten beginnen met een
+    //       kleine letter?
+    $sql = "INSERT INTO Lid (LidID, Voornaam, Tussenvoegsel, Geboortedatum,
+                              Adres, Woonplaats, Postcode, Geslacht,
+                              Emailadres, Rekeningnummer, Noodnummer, T-shirtmaat,
+                              Medicatie, Dieetwensen, Opmerking, ZHTC-emailadres)
+            VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array($_POST["LidID"],$_POST["Voornaam"],$_POST["Tussenvoegsel"],$_POST["Geboortedatum"],
+                         $_POST["Adres"],$_POST["Woonplaats"],$_POST["Postcode"],$_POST["Geslacht"],
+                         $_POST["Emailadres"],$_POST["Rekeningnummer"],$_POST["Noodnummer"],$_POST["shirtmaat"],
+                         $_POST["Medicatie"],$_POST["Dieetwensen"],$_POST["Opmerking"],$_POST["emailadres"]));
 }
-// Filter_input zorgt ervoor dat 1) de informatie gefilterd wordt en 2) de informatie 'veilig' is.
+// Filter_input zorgt ervoor dat 1) de informatie gefilterd wordt en 2) de informatie 'veilig' is. //NOTE: hoe dan?
 
 if (isset($_SESSION['email'])) {
     print($_SESSION['email']);
