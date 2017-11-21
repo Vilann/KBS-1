7be6377d2763a5a4024def7d0f77fe617bf0768b
Gentleman27 <32332886+Gentleman27@users.noreply.github.com>
2017-11-21 11:19:46 +0100

    Maak begin layout

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+		integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		<link rel="stylesheet" href="header.css">
+	</head>
+	<body>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+		<div class="container">
+			<img src="Website-logo-blauw.png" width="140px">
+		</div>
+
+		<nav>
+			<ul class="navigation">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="#">De vereniging</a></li>
+				<li><a href="sidebar_commissie.php">Commissies</a></li>
+				<li><a href="disputen.php">Disputen</a></li>
+				<li><a href="#">Activiteiten</a></li>
+				<li id="lustrum"><a href="#">Lustrum</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</body>
+</html>
