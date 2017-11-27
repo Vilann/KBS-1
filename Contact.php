b6100dce14e78989dc09abea122ca761c20281b1
Vilann <jelle.santema@gmail.com>
2017-11-27 13:14:14 +0100

    verwijder tweede iframe

--- Contact.php
+++ Contact.php
@@ -7,12 +7,6 @@
 
     <body>
         <h1>Contactformulier</h1>
-<<<<<<< HEAD
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.997172364323!2d6.100033315984932!3d52.515390244322994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7df3c9a5bcfe1%3A0x907105d2484be27f!2sAlgemene+Studentenvereniging+ZHTC!5e0!3m2!1snl!2snl!4v1511780795999" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        <div >Test Banana</div>
-=======
->>>>>>> 6ef298c9a975a4be4fb71ce65022bd0f3f43be72
 
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.997172364323!2d6.100033315984932!3d52.515390244322994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7df3c9a5bcfe1%3A0x907105d2484be27f!2sAlgemene+Studentenvereniging+ZHTC!5e0!3m2!1snl!2snl!4v1511780795999" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         <p>Test banaan</p>
