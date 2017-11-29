07ea6d3e6f25d06af2687055c330603bd8b0ff89
ThyJelmar <32633149+ThyJelmar@users.noreply.github.com>
2017-11-29 15:53:34 +0100

    Toevoegen account dropdown

--- includes/header.php
+++ includes/header.php
@@ -16,6 +16,34 @@
 			<img src="Website-logo-blauw.png" width="140px">
 		</div>
 
+		<nav class="navbar navbar-expand-lg navigation">
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNav">
+		    <ul class="navbar-nav mx-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">De vereniging</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="sidebar_commissie.php">Commissies</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="disputen.php">Disputen</a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link" href="disputen.php">Disputen</a>
+		      </li>
+					<li class="nav-item">
+		        <a id="lustrum" class="nav-link" href="#">Lustrum</a>
+		      </li>
+					<li class="nav-item">
+		        <a class="nav-link" href="contact.php">Contact</a>
+		      </li>
+		<!--
 		<nav>
 			<ul class="navigation">
 				<li><a href="index.php">Home</a></li>
@@ -25,16 +53,42 @@
 				<li><a href="#">Activiteiten</a></li>
 				<li id="lustrum"><a href="#">Lustrum</a></li>
 				<li><a href="contact.php">Contact</a></li>
-
+			-->
 				<?php
                 session_start();
                 if (!isset($_SESSION['email'])) {
                     print("<li><a href='login.php'>Log in/registreer</a></li>");
                 } else {
-                    print("<li><a href='#'>Hallo, met je email " . $_SESSION['email'] . " </a></li>");
+										print("<li class='nav-item dropdown'>
+						        <a class='nav-link dropdown-toggle' href='#' id='navbarDropdown' role='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+										<img src='http://via.placeholder.com/30x30'>"." ".$_SESSION['email']."
+										</a>
+						        <div class='dropdown-menu' aria-labelledby='navbarDropdown'>
+										<div class='container'>
+											<div class='row'>
+												<div class='col'>
+													<p class='small text-secondary'>Persoonlijke pagina's:</p>
+													<div class='dropdown-divider'></div>
+								          <p class='dropdown-item'>LidNiveau:	<span class='text-secondary'>Nieuwlid</span></p>
+								          <a href='#' class='dropdown-item'>Account</a>
+													<a href='#' class='dropdown-item'>Disputen</a>
+													<a href='#' class='dropdown-item'>Commissies</a>
+													<div class='dropdown-divider'></div>
+													<a href='#' class='btn btn-outline-danger mx-auto'><i class='fa fa-sign-out' aria-hidden='true'></i> <b>Afmelden</b></a>
+												</div>
+											</div>
+						        </div>
+										</div>
+						      </li>");
+                    //print("<li><a href='#'>Hallo, met je email " . $_SESSION['email'] . " </a></li>");
                 }
         ?>
 			</ul>
+		</div>
+	</nav>
+				<!--
+			</ul>
 		</nav>
+	-->
 	</body>
 </html>
