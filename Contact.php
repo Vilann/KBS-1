b4b8869ef3773b9a9b7bfd354e60d41fac407de6
ThyJelmar <32633149+ThyJelmar@users.noreply.github.com>
2017-11-29 09:39:47 +0100

    Bootstrap grid contact.php

--- Contact.php
+++ Contact.php
@@ -6,47 +6,56 @@
 
 
     <body>
-        <h1>Contactformulier</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <h1>Contactformulier</h1>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.997172364323!2d6.100033315984932!3d52.515390244322994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7df3c9a5bcfe1%3A0x907105d2484be27f!2sAlgemene+Studentenvereniging+ZHTC!5e0!3m2!1snl!2snl!4v1511780795999" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+          <p>Test banaan</p>
+        </div>
+          <div class="col">
+            <form Method="POST" Action='contact.php'>
+              <table>
+                <tr>
+                  <td><label for="contactmail">Emailadres:</label></td>
+                  <td><input id="contactmail" type="mail" name="contactmail" placeholder="Uwmailadres@voorbeeld.com" required></td>
+                </tr>
+                <tr>
+                  <td><label for="contactnaam">Naam:</label></td>
+                  <td><input id="contactnaam" type="text" name="contactnaam" placeholder="Uw naam" required></td>
+                </tr>
+                <tr>
+                  <td><label for="contactbericht">Bericht:</label></td>
+                  <td><textarea id="contactbericht" name="contactbericht" placeholder="Uw vragen, opmerkingen of tips" required></textarea></td>
+                </tr>
+              </table>
+                <input type="submit" name="verzend" value="Verzend">
+            </form>
+            <?php
+            if (isset($_POST['verzend'])) {
+                print_r($_POST);
+            }
+            if (isset($_POST['verzend']) && isset($_POST['contactmail']) && isset($_POST['contactnaam']) && isset($_POST['contactbericht'])) {
+                $naam = $_POST['contactnaam'];
+                $emailadres = $_POST['contactmail'];
+                $bericht = $_POST['contactbericht'];
+                $zhtcmailadres = "Iemands@emailadres.com";
+                $onderwerp = "Een mail van $naam";
+                $mailbericht = "$naam heeft het volgende verstuurd: $bericht het emailadres van $naam is $emailadres";
+                mail($zhtcmailadres, $onderwerp, $mailbericht);
+                print("hoho klopt ... $mailbericht je ip is $ip");
+            };
+            print("hoho klopt ... $mailbericht");
+            ?>
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.997172364323!2d6.100033315984932!3d52.515390244322994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7df3c9a5bcfe1%3A0x907105d2484be27f!2sAlgemene+Studentenvereniging+ZHTC!5e0!3m2!1snl!2snl!4v1511780795999" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        <p>Test banaan</p>
-
-        <form Method="POST" Action='contact.php'>
-          <table>
-            <tr>
-              <td><label for="contactmail">Emailadres:</label></td>
-              <td><input id="contactmail" type="mail" name="contactmail" placeholder="Uwmailadres@voorbeeld.com" required></td>
-            </tr>
-            <tr>
-              <td><label for="contactnaam">Naam:</label></td>
-              <td><input id="contactnaam" type="text" name="contactnaam" placeholder="Uw naam" required></td>
-            </tr>
-            <tr>
-              <td><label for="contactbericht">Bericht:</label></td>
-              <td><textarea id="contactbericht" name="contactbericht" placeholder="Uw vragen, opmerkingen of tips" required></textarea></td>
-            </tr>
-          </table>
-            <input type="submit" name="verzend" value="Verzend">
-        </form>
-        <?php
-        if (isset($_POST['verzend'])) {
-            print_r($_POST);
-        }
-        if (isset($_POST['verzend']) && isset($_POST['contactmail']) && isset($_POST['contactnaam']) && isset($_POST['contactbericht'])) {
-            $naam = $_POST['contactnaam'];
-            $emailadres = $_POST['contactmail'];
-            $bericht = $_POST['contactbericht'];
-            $zhtcmailadres = "Iemands@emailadres.com";
-            $onderwerp = "Een mail van $naam";
-            $mailbericht = "$naam heeft het volgende verstuurd: $bericht het emailadres van $naam is $emailadres";
-            mail($zhtcmailadres, $onderwerp, $mailbericht);
-            print("hoho klopt ... $mailbericht je ip is $ip");
-        };
-        print("hoho klopt ... $mailbericht");
-        ?>
-
-
-
-
+        </div>
+      </div>
+      </div>
     </body>
 </html>
