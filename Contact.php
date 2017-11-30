cd2cbb6df38860d67456b850ed8bbbc66d76337b
glsgithub <32291548+glsgithub@users.noreply.github.com>
2017-11-30 13:27:24 +0100

    Verwijder code

--- Contact.php
+++ Contact.php
@@ -38,8 +38,7 @@
                 <input type="submit" name="verzend" value="Verzend">
             </form>
             <?php
-            if (isset($_POST['verzend'])) {
-                print_r($_POST);
+          
             }
             if (isset($_POST['verzend']) && isset($_POST['contactmail']) && isset($_POST['contactnaam']) && isset($_POST['contactbericht'])) {
                 $naam = $_POST['contactnaam'];
@@ -49,9 +48,9 @@
                 $onderwerp = "Een mail van $naam";
                 $mailbericht = "$naam heeft het volgende verstuurd: $bericht het emailadres van $naam is $emailadres";
                 mail($zhtcmailadres, $onderwerp, $mailbericht);
-                print("hoho klopt ... $mailbericht je ip is $ip");
+                print("hoho klopt ... $mailbericht ");
             };
-            print("hoho klopt ... $mailbericht");
+
             ?>
 
         </div>
--- contactverwerk.php
+++ contactverwerk.php
@@ -1,12 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ZHTC - Login</title>
+    <?php
+$nu=time();
+for ($inlogpoging=0; $inlogpoging>3;$inlogpoging++) {
+};
+while ($nu<$kwartier) {
+    $inlog=true;
+}
+$ip = getenv("REMOTE_ADDR") ;
+$datum= date('Y-m-d');
+$kwartier=time() + (0 * 0 * 15 * 00);
+print("Hallo de tijd is  $kwartier". date('Y-m-d'));
+print("$ip");
 
-$db = "mysql:host=localhost;dbname=ZHTC;port=3306";
-$user = "root";
-$pass = "";
-$pdo = new PDO($db, $user, $pass);
-// We halen het wachtwoord op van het lid met het lidID dat bij het emailadres staat.
-$stmt = $pdo->prepare("INSERT INTO login (lidID, wachtwoord, datecreated)
-                       VALUES (?,?,?)");
-$stmt->execute(array(1, password_hash("geheimwachtwoord", PASSWORD_BCRYPT), date("Y-m-d")));
 
 //|| empty ($emailadres) || (empty ($bericht))
