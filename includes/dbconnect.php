f61b6378e4a360921614f90c27eed3f5c5ce4c36
ThyJelmar <32633149+ThyJelmar@users.noreply.github.com>
2017-12-20 00:17:43 +0100

    merge markers weg

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,6 +1,5 @@
 <?php
 try {
-<<<<<<< HEAD
 	$db = "mysql:host=localhost;dbname=zhtc;port=3306";
 	$user = "root";
 	$pass = "";
@@ -8,12 +7,4 @@ try {
 }
 catch (PDOException $e) {
 echo $e->getTraceAsString();
-=======
-    $db = "mysql:host=localhost;dbname=zhtc;port=3306";
-    $user = "root";
-    $pass = "";
-    $pdo = new PDO($db, $user, $pass);
-} catch (PDOException $e) {
-    echo $e->getTraceAsString();
->>>>>>> b37c2c78df3625e351ab3b94f55f3bde5e9204f3
 }
