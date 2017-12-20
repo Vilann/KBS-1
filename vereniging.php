643db656fc4f35c0efa71910da4466dc4cd8ad21
glsgithub <32291548+glsgithub@users.noreply.github.com>
2017-12-20 23:03:50 +0100

    Maak vereniging

--- vereniging.php
+++ vereniging.php
@@ -11,59 +11,133 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <h1 class="text-center mt-4">Over ZHTC</h1>
+          <h1 class="text-center mx-3">Over ZHTC</h1>
           <!-- <p class="text-muted"><i class="icon ion-chevron-left"></i> oude commissies</p> -->
         </div>
+			</div>
       </div>
 			<hr>
-			<p class="text-center mt-4">Algemene studentenvereniging Zwols Hoger Technisch Corps (Asv. ZHTC) is dé studentenvereniging voor iedere HBO – òf universitaire student in Zwolle.
+			<div class="container-fluid">
+	      <div class="row">
+	        <div class="col-12">
+			<p class="text-center text-muted font-weight-light mx-3">Algemene studentenvereniging Zwols Hoger Technisch Corps (Asv. ZHTC) is dé studentenvereniging voor iedere HBO – òf universitaire student in Zwolle.
 Asv. ZHTC heeft als enige studentenvereniging de beschikking over een eigen sociëteit. Een mooi en groot pand aan de Thomas à Kempisstraat, net buiten de grachten van het centrum. Mede dankzij deze sociëteit zorgt Asv. ZHTC voor een leuke, gezellige en ook nog eens een leerzame studententijd. De vereniging is opgericht in 1952, heeft ongeveer driehonderd leden en donateurs en is daarmee de oudste en gezelligste studentenvereniging van Zwolle. De vereniging is opgericht met koninklijke goedkeuring, op d.d. 21 November 1952 en ingeschreven in het verenigingsregister bij de Kamer van Koophandel te Zwolle onder nummer 40059409.</p>
+</div>
+</div>
+</div>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<h1 class="text-center mt-4">Bestuur</h1>
+
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid">
+				<div class="row">
+					<img src="Website-logo-blauw.png" class="mx-auto d-block img-responsive" width="10%" height="14%">
+				</div>
+			</div>
+				<hr>
+			<p class="text-center text-muted font-weight-light mx-3">Het bestuur der A.s.v. ZHTC is het overkoepelend orgaan van de vereniging. Zij zorgt ervoor dat alle zaken binnen de vereniging naar wens verlopen. Hierbij kan gedacht worden aan het onderhouden van relaties, het beheren van de liquide middelen, maar ook het coördineren van de verscheidene commissies welke onze vereniging rijk is. Deze taken maken dat het bestuur de verantwoordelijkheid draagt voor de koers van de vereniging, welke zij dan ook meerdere malen aflegt tijdens ALV’s.
+
+Op dit moment bestaat het bestuur uit: (van links naar rechts)
+– Peter Beening (secretaris)
+– Bob Kiers(voorzitter)
+– Gabriël Boekhold (penningmeester)
+– Willemien Pilkes(vice-voorzitter) </p>
+<div class="container_fluid">
+	<div class="row">
+		<div class="col-12">
+			<h1 class="text-center mt-4">Het Gilde</h1>
+		</div>
+	</div>
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+<p class="text-center text-muted font-weight-light mx-3">
+	Na een aantal zeer interessante brainstormsessies met enkele donateurs, diverse feesten en activiteiten die daar uit voortvloeiden worden deze donateursactiviteiten geformaliseerd middels de oprichting van Het Gilde. We willen circa maandelijks activiteiten organiseren. Deze activiteiten bestaan uit reeds bestaande successen (o.a. de bierweekvrijdag, maar ook de druiffuif en spit piggy zijn recent weer terug gekomen), dispuutsfeesten, bierproeverijen en nieuwe activiteiten. Bij deze laatste denken we onder andere ook aan opleidingsgerichte netwerkborrels.
+
+Voor meer informatie zie<a href=http://www.zhtc.nl/gilde>http://www.zhtc.nl/gilde</a> of onze Facebook. Hierop komt onder andere informatie over de diverse activiteiten te staan, maar komt er ook ruimte voor uw ZHTC-belevenissen, bijvoorbeeld onder het kopje anekdotes.
+
+Samen kunnen we er een prachtig platform/ZHTC-archief van maken!
+
+Hopelijk tot ziens bij een van onze activiteiten.
+
+Emile Hoogterp
+Voorzitter oprichtingsbestuur Het Gilde.
+<a href=http://www.zhtc.nl/gilde>http://www.zhtc.nl/gilde</a>
+Facebook
+</p>
+</div>
+</div>
+</div>
+
+
+			<div class="container_fluid">
+				<div class="row">
+					<div class="col-12">
+						<h1 class="text-center mt-4">Geschiedenis</h1>
+
+					</div>
+				</div>
+			</div>
+			<div class="container-fluid">
+	      <div class="row">
+	        <div class="col-12">
+					<p class="text-center text-muted font-weight-light mx-3">	De initiatiefnemer voor de oprichting van de Zwolse Middelbare Technische School was Ir. F.S. Langemeijer. Hij was het die de grote vraag naar leidinggevend personeel in de regionale industrie onderkende, veroorzaakt door de naoorlogse stijging van de conjunctuur.</p>
+			<p class="text-center text-muted font-weight-light mx-3">Lees <a href=https://zhtc.nl/gilde/historie/zhtc-geschiedenis>hier</a> meer over de geschiedenis van ZHTC.</p>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-12">
+			<h1 class="text-center mt-4">Oud-Besturen</h1>
+
+		</div>
+	</div>
+</div>
 
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-12">
-						<h1 class="text-center mt-4">Geschiedenis</h1>
-						<!-- <p class="text-muted"><i class="icon ion-chevron-left"></i> oude commissies</p> -->
-					</div>
-				</div>
-				<hr>
-			<p class="text-center mt-4"> Info... </p>
-			<div class="container_fluid">
-				<div class="row">
-					<div class="col-12">
-						<h1 class="text-center mt-4">Oud-besturen</h1>
-						<!-- <p class="text-muted"><i class="icon ion-chevron-left"></i> oude commissies</p> -->
-					</div>
-				</div>
-				<hr>
-			<h4> Oud bestuur 1: </h4>
-			<p> Info... </p>
-			<h4> Oud bestuur 2: </h4>
-			<p> Info... </p>
-			<?php
 
-                    $stmt = $pdo->prepare('SELECT dispuutnaam, dispuutzin, dispuuttekst
-					FROM dispuut');
-                    $stmt->execute();
-                    $data = $stmt->fetchAll();
-                    foreach ($data as $row) {
-                        ?>
-      <div class="row">
-        <div class="col-12">
-          <div class="card mb4">
-            <div class="card-body"> <?php // NOTE: alles in deze div staat in de kaart?>
-              <h4 class="card-title"><?php print($row['dispuutnaam'])?></h4>
-              <p class="card-text">
-              <a href="#" class="btn btn-outline-primary float-right zhtc-button">Meer <i class="icon ion-arrow-right-c"></i></a>
-							<img class="card-img-left" src="afentikabanner.jpg" alt="" width="180px"><?php print($row['dispuutzin']); ?></p>
+			<h4 class="text-center mt-4"> Oud bestuur 2015-2016: </h4>
+			<ul class="text-center text-muted font-weight-light mx-3">
+				<li> Voorzitter: Emile Hoogterp</li>
+				<li> Secretaris: Wieger van der Werf</li>
+				<li> Penningmeester: Hans Blom</li>
+				<li> Algemeen-secundus: Olaf Brugman</li>
+				<li> Algemeen-secundus: Dirk Schut</li>
+				<li> Algemeen-secundus: Ragni Wennemers</li>
+				<li> Algemeen-secundus: Joost Leliveld</li>
+				<li> Algemeen-secundus: Mark van Duuren</li>
+				 </ul>
+				 <h4 class="text-center mt-4"> Oud bestuur 2014-2015: </h4>
+				 <ul class="text-center text-muted font-weight-light mx-3">
+			<li>Voorzitter: Emile Hoogterp</li>
+			<li>Secretaris: Martijn van der Vlag</li>
+			<li>Penningmeester: Hans Blom</li>
+			<li>Algemeen-secundus: Olaf Brugman</li>
+			<li>Algemeen-secundus: Dirk Schut</li>
+		</ul>
+		<h4 class="text-center mt-4"> Oud bestuur 2013-2014: </h4>
+		<ul class="text-center text-muted font-weight-light mx-3">
+		<li>Voorzitter: Emile Hoogterp</li>
+		<li>Secretaris: Merel van Rees</li>
+		<li>Penningmeester: Hans Blom</li>
+		<li>Algemeen-secundus: Martijn van der Vlag</li>
+		<li>Algemeen-secundus: Olaf Brugman</li>
+		<li>Algemeen-secundus: Martine van der Elsen </li>
+	</ul>
+
             </div>
           </div>
         </div>
-      </div>
-    </div>
+
+
   <?php include 'includes/footer.php'; ?>
-  <script>
-  <?php
-                    } include 'includes/script.js'; ?>
-  </script>
+
 </html>
