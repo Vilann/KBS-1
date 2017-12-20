53cf5fde28cf675222c526b5f405ee9e489c7347
Gentleman27 <32332886+Gentleman27@users.noreply.github.com>
2017-12-20 09:35:41 +0100

    werkt wel

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
               include 'includes/dbconnect.php';
               //include 'includes/sidebar.php';?>
               <!-- This script triggers the modal without a button -->
-              <script type="text/javascript">
+              <!-- <script type="text/javascript">
               	$(document).ready(function(){
               		$("#myModal").modal('show');
               	});
-              </script>
+              </script> -->
               <script>(function(d, s, id) {
                 var js, fjs = d.getElementsByTagName(s)[0];
                 if (d.getElementById(id)) return;
