cab5ed730d6be3b8c5318d56ea9da04a572c6348
Vilann <jelle.santema@gmail.com>
2018-01-07 16:36:46 +0100

    fix footer, is af nu

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
-	<footer class="card-footer text-muted">
+	<footer class="card-footer text-muted container-fluid">
+<div class="row">
+	<div class="col-4">
 
 	<h4>CONTACT INFO
 </h1>
@@ -12,22 +14,29 @@ Postbus 1475, 8001 BL, Zwolle
 EMAIL
 </h6>
 <p>secretariaat@zhtc.nl</p>
-
-<!-- <div>
-<h4>Sponsoren</h4> -->
+<h6>
+TELEFOON
+</h6>
+<p>06 373 424 59</p>
+</div>
+<div class="col-8">
+<h4>SPONSOREN</h4>
 <?php
-/*
-include("includes/dbconnect.php");
-$stmt = $pdo->prepare("SELECT * FROM sponsor");
-$sponsor = $stmt->fetch(PDO::FETCH_ASSOC);
-if ($stmt->rowCount()) {
-    foreach ($variable as $key => $value) {
-        ?>
-    <img src=<?php print($sponsor['sponsorplaatje']);
-    }
-}*/?>
 
-<!-- </div> -->
+include("includes/dbconnect.php");
+$stmt = $pdo->prepare("SELECT * FROM sponsor LIMIT 4");
+$stmt->execute();
+$sponsoren = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($stmt->rowCount() > 0) {
+    foreach ($sponsoren as $sponsor) {
+        print("<img style='margin-left: 1em' src='" . $sponsor['sponsorplaatje'] . "'>");
+    }
+} else {
+    die($sponsor);
+}
+?>
+</div>
+</div>
 </footer>
 </body>
 </html>
